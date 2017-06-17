e5032847da7f48239841dbabd121b5c009b89fde
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-17 19:57:03 +0900

    server

--- sample.js
+++ sample.js
@@ -1,1 +1,7 @@
-/*test*/
+http = require("http");
+PORT = 8124;
+
+http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!\n");
+  }).listen(PORT);
