9434ec0c6ea2448196448aab8b81c9eb5d9fc661
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-17 22:08:20 +0900

    add index.html

--- sample.js
+++ sample.js
@@ -1,4 +1,6 @@
 var http = require("http");
+// FileStream.  fs.readFile(filePath, encode, callback) でファイルの読み込み.
+var fs = require("fs");
 var port = process.env.PORT || 5000;
 
 var server = http.createServer();
@@ -6,11 +8,13 @@ server.on("request", onRequest);
 server.listen(port, startServerAlert);
 
 function onRequest(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write("Hello, World!\n");
-  res.end();
+    fs.readFile("index.html", "UTF-8", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Hello, World!\n");
+        res.end();
+    });
 }
 
 function startServerAlert() {
-  console.log('Server running at http: ${port}/');
+    console.log('Server running at http: ${port}/');
 }
