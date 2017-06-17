41b4499f01e037b8b533eacf8f512494e4ee4325
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-17 20:06:58 +0900

    remove hostaddr

--- sample.js
+++ sample.js
@@ -1,11 +1,16 @@
 var http = require("http");
-var PORT = 8124;
+var hostaddr = "localhost";
+var port = process.env.PORT || 5000;
 
 var server = http.createServer();
 server.on("request", onRequest);
-server.listen(port, hostaddr, startServerAlert);
+server.listen(port, startServerAlert);
 
 function onRequest(req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello, World!\n");
+}
+
+function startServerAlert() {
+  console.log('Server running at http: //${hostaddr}:${port}/');
 }
