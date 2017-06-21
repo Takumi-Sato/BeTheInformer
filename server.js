3a963a0b9e538f568868a6a9faba6082e8beb299
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-21 14:24:33 +0900

    fix for express

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
-var http = require("http");
 var express = require("express");
 // FileStream.  fs.readFile(filePath, encode, callback) でファイルの読み込み.
-var fs = require("fs");
 var port = process.env.PORT || 5000;
 
 var app = express.createServer();
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
   res.sendfile("index.html");
 });
 app.listen(port);
-
-function onRequest(req, res) {
-    fs.readFile("index.html", "UTF-8", function(err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-    });
-}
-
-function startServerAlert() {
-    console.log('Server running at http: ${port}/');
-}
