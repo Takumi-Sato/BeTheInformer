96a4c46bea52d465ca6d8b98849359e3b615bfcd
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-23 17:22:14 +0900

    initialize gp

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var port = process.env.PORT || 5000;
 
 var app = express();
 
+var pg = require('pg');
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT * FROM Player;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
 app.set("port", port);
 //app.use(express.static(__dirname + '/public'));
 
@@ -13,3 +27,4 @@ app.get('/', function(req, res) {
 app.listen(app.get("port"), function () { 
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
