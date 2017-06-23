76cda83b2995cbfbae8220cc3c779de700644295
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-23 18:04:19 +0900

    revert

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require("express");
 // FileStream.  fs.readFile(filePath, encode, callback) でファイルの読み込み.
 var port = process.env.PORT || 5000;
-var router = express.Router();
 
 var app = express();
-
-//var returnToClient = require("./routes/returnToClient");
-//app.use("/returnToClient", returnToClient);
 
 var pg = require('pg');
 pg.defaults.ssl = true;
@@ -23,7 +19,6 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 
 
 app.set("port", port);
-//app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendfile("index.html");
@@ -32,4 +27,3 @@ app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-//module.exports = router;
