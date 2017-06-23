23fe923578b39b4984245302ebf2ab6bc1a01ba6
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-23 18:01:14 +0900

    fix

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var router = express.Router();
 
 var app = express();
 
-var returnToClient = require("./routes/returnToClient");
-app.use("/returnToClient", returnToClient);
+//var returnToClient = require("./routes/returnToClient");
+//app.use("/returnToClient", returnToClient);
 
 var pg = require('pg');
 pg.defaults.ssl = true;
@@ -32,4 +32,4 @@ app.listen(app.get("port"), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-module.exports = router;
+//module.exports = router;
