66be0ead89cdd65d34feda74d8aa685f7d187e36
Takumi-Sato <s1511378@coins.tsukuba.ac.jp>
2017-06-28 13:04:28 +0900

    server edit

--- server.js
+++ server.js
@@ -9,25 +9,28 @@ var app = express();
 
 var pg = require('pg');
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT * FROM PlayerTest;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 
 app.set("port", port);
 
 app.get('/', function(req, res) {
-  res.sendfile("index.html");
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        if (err) throw err;
+        console.log('Connected to postgres! Getting schemas...');
+
+
+        var query = 'SELECT * FROM PlayerTest;';
+        query.on('row', function(row) {
+            console.log(JSON.stringify(row));
+            res.sendfile("index.html");
+        });
+        query.on('error', function(err) {
+            console.log("ERROR");
+        });
+    });
+    //res.sendfile("index.html");
 });
-app.listen(app.get("port"), function () { 
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen(app.get("port"), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
 
 module.export = router;
